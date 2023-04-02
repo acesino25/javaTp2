1dd4777b77b0a91f56a621487c7da70b1c8cc86b
acesino25 <acesino25@hotmail.com>
2023-04-02 08:46:26 -0300

    Agregado ejercicio3

---
+++ ejercicio3/Votante.java
@@ -0,0 +1,26 @@
+// Declaramos la clase
+
+public class Votante{
+    // declaramos dos valores privados
+    // que no tendrán método modificador
+    private int edad;
+
+    static private int EDADMINIMA = 18;
+    static private int EDADMAXIMA = 63;
+
+    // Declaramos el constructor
+
+    public Votante(int edad){
+        this.edad = edad;
+    }
+
+    // Declaramos un getter para devolver condicionalmente una respuesta
+    public void ComprobarEdad(){
+        if(this.edad > EDADMINIMA && this.edad < EDADMAXIMA){
+            System.out.println("Votante habilitado")
+        }else{
+            System.out.println("Votante inhabilitado")
+        }
+    }
+
+}
